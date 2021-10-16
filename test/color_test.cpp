53c85f388d589e7da9dcc2ad3c40976ca107bc56
MCWertGaming <dleven@protonmail.com>
2021-10-16 20:47:19 +0000

    split tests into smaller test suits

---
+++ test/color_test.cpp
@@ -0,0 +1,109 @@
+#include <foxterm/color.hpp>
+#include <gtest/gtest.h>
+
+TEST(color_test, reset) { EXPECT_STREQ(Foxterm::reset(), "\033[m"); }
+
+TEST(color_test, 4bit_color_test) {
+  // foreground
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::black), "\033[30m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::blue), "\033[34m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::blue_bright), "\033[94m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::cyan), "\033[36m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::cyan_bright), "\033[96m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::gray), "\033[90m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::green), "\033[32m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::green_bright), "\033[92m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::magenta), "\033[35m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::magenta_bright),
+            "\033[95m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::red), "\033[31m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::red_bright), "\033[91m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::reset), "\033[39m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::white), "\033[37m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::white_bright), "\033[97m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::yellow), "\033[33m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_fg::yellow_bright), "\033[93m");
+  // background
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::black), "\033[40m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::blue), "\033[44m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::blue_bright), "\033[104m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::cyan), "\033[46m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::cyan_bright), "\033[106m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::gray), "\033[100m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::green), "\033[42m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::green_bright), "\033[102m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::magenta), "\033[45m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::magenta_bright),
+            "\033[105m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::red), "\033[41m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::red_bright), "\033[101m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::reset), "\033[49m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::white), "\033[47m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::white_bright), "\033[107m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::yellow), "\033[43m");
+  EXPECT_EQ(Foxterm::get_color4(Foxterm::Color4_bg::yellow_bright),
+            "\033[103m");
+}
+TEST(color_test, 8bit_colors) {
+  // foreground
+  EXPECT_EQ(Foxterm::get_color8_fg(0), "\033[38;5;0m");
+  EXPECT_EQ(Foxterm::get_color8_fg(10), "\033[38;5;10m");
+  EXPECT_EQ(Foxterm::get_color8_fg(255), "\033[38;5;255m");
+  EXPECT_EQ(Foxterm::get_color8_fg(99), "\033[38;5;99m");
+  // background
+  EXPECT_EQ(Foxterm::get_color8_bg(0), "\033[48;5;0m");
+  EXPECT_EQ(Foxterm::get_color8_bg(10), "\033[48;5;10m");
+  EXPECT_EQ(Foxterm::get_color8_bg(255), "\033[48;5;255m");
+  EXPECT_EQ(Foxterm::get_color8_bg(99), "\033[48;5;99m");
+  // underline
+  EXPECT_EQ(Foxterm::get_color8_underline(0), "\033[58;5;0m");
+  EXPECT_EQ(Foxterm::get_color8_underline(255), "\033[58;5;255m");
+  EXPECT_EQ(Foxterm::get_color8_underline(30), "\033[58;5;30m");
+}
+TEST(color_test, 24bit_colors) {
+  // foreground
+  EXPECT_EQ(Foxterm::get_color24_fg(0, 0, 0), "\033[38;2;0;0;0m");
+  EXPECT_EQ(Foxterm::get_color24_fg(255, 255, 255), "\033[38;2;255;255;255m");
+  EXPECT_EQ(Foxterm::get_color24_fg(20, 20, 20), "\033[38;2;20;20;20m");
+  EXPECT_EQ(Foxterm::get_color24_fg(3, 4, 5), "\033[38;2;3;4;5m");
+  // background
+  EXPECT_EQ(Foxterm::get_color24_bg(0, 0, 0), "\033[48;2;0;0;0m");
+  EXPECT_EQ(Foxterm::get_color24_bg(255, 255, 255), "\033[48;2;255;255;255m");
+  EXPECT_EQ(Foxterm::get_color24_bg(20, 20, 20), "\033[48;2;20;20;20m");
+  EXPECT_EQ(Foxterm::get_color24_bg(3, 4, 5), "\033[48;2;3;4;5m");
+  // underline
+  EXPECT_EQ(Foxterm::get_color24_underline(0, 0, 0), "\033[58;2;0;0;0m");
+  EXPECT_EQ(Foxterm::get_color24_underline(255, 255, 255),
+            "\033[58;2;255;255;255m");
+  EXPECT_EQ(Foxterm::get_color24_underline(20, 20, 20), "\033[58;2;20;20;20m");
+  EXPECT_EQ(Foxterm::get_color24_underline(3, 4, 5), "\033[58;2;3;4;5m");
+}
+TEST(color_test, 24bit_colors_struct) {
+  // foreground
+  EXPECT_EQ(Foxterm::get_color24_fg(Foxterm::make_rgb(0, 0, 0)),
+            "\033[38;2;0;0;0m");
+  EXPECT_EQ(Foxterm::get_color24_fg(Foxterm::make_rgb(255, 255, 255)),
+            "\033[38;2;255;255;255m");
+  EXPECT_EQ(Foxterm::get_color24_fg(Foxterm::make_rgb(20, 20, 20)),
+            "\033[38;2;20;20;20m");
+  EXPECT_EQ(Foxterm::get_color24_fg(Foxterm::make_rgb(3, 4, 5)),
+            "\033[38;2;3;4;5m");
+  // background
+  EXPECT_EQ(Foxterm::get_color24_bg(Foxterm::make_rgb(0, 0, 0)),
+            "\033[48;2;0;0;0m");
+  EXPECT_EQ(Foxterm::get_color24_bg(Foxterm::make_rgb(255, 255, 255)),
+            "\033[48;2;255;255;255m");
+  EXPECT_EQ(Foxterm::get_color24_bg(Foxterm::make_rgb(20, 20, 20)),
+            "\033[48;2;20;20;20m");
+  EXPECT_EQ(Foxterm::get_color24_bg(Foxterm::make_rgb(3, 4, 5)),
+            "\033[48;2;3;4;5m");
+  // underline
+  EXPECT_EQ(Foxterm::get_color24_underline(Foxterm::make_rgb(0, 0, 0)),
+            "\033[58;2;0;0;0m");
+  EXPECT_EQ(Foxterm::get_color24_underline(Foxterm::make_rgb(255, 255, 255)),
+            "\033[58;2;255;255;255m");
+  EXPECT_EQ(Foxterm::get_color24_underline(Foxterm::make_rgb(20, 20, 20)),
+            "\033[58;2;20;20;20m");
+  EXPECT_EQ(Foxterm::get_color24_underline(Foxterm::make_rgb(3, 4, 5)),
+            "\033[58;2;3;4;5m");
+}
---
+++ test/style_test.cpp
@@ -0,0 +1,83 @@
+#include <foxterm/style.hpp>
+#include <gtest/gtest.h>
+
+TEST(style_test, supported) {
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::reset), "\033[m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::bold), "\033[1m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::dim), "\033[2m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::disable_bold_dim), "\033[22m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::disable_invert), "\033[27m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::disable_italic), "\033[23m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::invert), "\033[7m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style::italic), "\033[3m");
+}
+TEST(style_test, extra) {
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::hide), "\033[8m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::disable_underline),
+            "\033[24m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::dissable_strike),
+            "\033[29m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::reveal), "\033[28m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::strike), "\033[9m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::underline), "\033[4m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_extra::underline_double),
+            "\033[21m");
+}
+TEST(style_test, unsupported) {
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_1), "\033[11m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_2), "\033[12m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_3), "\033[13m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_4), "\033[14m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_5), "\033[15m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_6), "\033[16m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_7), "\033[17m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_8), "\033[18m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_9), "\033[19m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_default),
+            "\033[10m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::blink_rapid),
+            "\033[6m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::blink_slow),
+            "\033[5m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::disable_blinking),
+            "\033[25m");
+  EXPECT_EQ(
+      Foxterm::get_style(Foxterm::Style_unsupported::disable_framed_encircled),
+      "\033[54m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::disable_overline),
+            "\033[55m");
+  EXPECT_EQ(Foxterm::get_style(
+                Foxterm::Style_unsupported::disable_proportional_spacing),
+            "\033[50m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::encircled),
+            "\033[52m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::font_fracture),
+            "\033[20m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::framed), "\033[51m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::overlined),
+            "\033[53m");
+  EXPECT_EQ(
+      Foxterm::get_style(Foxterm::Style_unsupported::proportional_spacing),
+      "\033[26m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::ideogram_underline),
+            "\033[60m");
+  EXPECT_EQ(
+      Foxterm::get_style(Foxterm::Style_unsupported::ideogram_underline_double),
+      "\033[61m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::ideogram_overline),
+            "\033[62m");
+  EXPECT_EQ(
+      Foxterm::get_style(Foxterm::Style_unsupported::ideogram_double_overline),
+      "\033[63m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::ideogram_stress),
+            "\033[64m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::disable_ideogram),
+            "\033[65m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::superscript),
+            "\033[73m");
+  EXPECT_EQ(Foxterm::get_style(Foxterm::Style_unsupported::subscript),
+            "\033[74m");
+  EXPECT_EQ(
+      Foxterm::get_style(Foxterm::Style_unsupported::disable_super_subscript),
+      "\033[75m");
+}
